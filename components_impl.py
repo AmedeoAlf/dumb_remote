6f4c648444cd51bf0d834d5bdb0e193023a01105
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2025-07-25 16:00:30 +0200

    Specify `device` as private (implementation detail)

--- components_impl.py
+++ components_impl.py
@@ -4,14 +4,14 @@ from scrollable import Scrollable
 LMB = uinput.BTN_LEFT
 RMB = uinput.BTN_RIGHT
 
-device = uinput.Device([uinput.REL_X, uinput.REL_Y, uinput.REL_WHEEL_HI_RES,
-                       uinput.REL_HWHEEL_HI_RES, uinput.BTN_LEFT, uinput.BTN_RIGHT])
+_device = uinput.Device([uinput.REL_X, uinput.REL_Y, uinput.REL_WHEEL_HI_RES,
+                         uinput.REL_HWHEEL_HI_RES, uinput.BTN_LEFT, uinput.BTN_RIGHT])
 
 
 class Touchpad(Scrollable):
     def _rel_move(self, dx: int, dy: int) -> None:
-        device.emit(uinput.REL_X, dx)
-        device.emit(uinput.REL_Y, dy)
+        _device.emit(uinput.REL_X, dx)
+        _device.emit(uinput.REL_Y, dy)
 
 
 class Mousewheel(Scrollable):
@@ -19,9 +19,9 @@ class Mousewheel(Scrollable):
         self.sensibility = (-12, 4)
 
     def _rel_move(self, dx: int, dy: int) -> None:
-        device.emit(uinput.REL_HWHEEL_HI_RES, dx)
-        device.emit(uinput.REL_WHEEL_HI_RES, dy)
+        _device.emit(uinput.REL_HWHEEL_HI_RES, dx)
+        _device.emit(uinput.REL_WHEEL_HI_RES, dy)
 
 
 def set_btn(button, pressed: bool):
-    device.emit(button, pressed)
+    _device.emit(button, pressed)
